d210c560cafc362a3bc4ee709d9f527842773c9e
Yashi Srivastava <139585327+yashiisri@users.noreply.github.com>
2025-07-31 13:37:50 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
     }
 
     try {
-      const res = await fetch('http://localhost:5000/api/register', {
+      const res = await fetch('https://finvista-backendd.onrender.com/api/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
